76e4cb2746c979c77130db91ec12ff2cbb078e3c
Saint-M <saint-martin_10@hotmail.com>
2022-06-21 00:47:32 -0500

    actualizamos detalles

--- script.js
+++ script.js
@@ -24,15 +24,15 @@ class Productos {
     }
 }
 
-const objeto1 = new Productos(0, "iphone 11 64gb", "apple", 10500, 0)
-const objeto2 = new Productos(1, "moto e20", "motorola", 5500, 2)
+const objeto1 = new Productos(0, "iphone 11 64gb", "apple", 8500, 0)
+const objeto2 = new Productos(1, "moto e20", "motorola", 2500, 2)
 const objeto3 = new Productos(2, "ssd 240gb", "wd green", 800, 6)
-const objeto4 = new Productos(3, "galaxy a12", "samsung", 3500, 3)
-const objeto5 = new Productos(4, "laptop lenovo thinkpad", "lenovo", 11000, 1)
-const objeto6 = new Productos(5, "galaxy a13", "samsung", 4500, 3)
+const objeto4 = new Productos(3, "galaxy a12", "samsung", 2999, 3)
+const objeto5 = new Productos(4, "laptop lenovo thinkpad", "lenovo", 9500, 1)
+const objeto6 = new Productos(5, "galaxy a13", "samsung", 3699, 3)
 const objeto7 = new Productos(6, "bateria portatil", "1hora", 1500, 4)
 const objeto8 = new Productos(7, "grafica rtx 2060", "msi", 8000, 1)
-const objeto9 = new Productos(8, "ssd 512gb", "xpg", 3000, 3)
+const objeto9 = new Productos(8, "ssd 512gb", "xpg", 1200, 3)
 const objeto10 = new Productos(9, "procesador ryzen 5 3600", "ryzen", 8500, 2)
 
 const deptoElectronica= [objeto1, objeto2, objeto3, objeto4, objeto5, objeto6, objeto7,objeto8, objeto9, objeto10]
@@ -79,6 +79,7 @@ fetch('productos.json')
 function inicializar() {
     selecProducto()
     seleCategorias()
+    selecRango()
     consultarCarrito()
     eliminarProducto()
 }
@@ -100,24 +101,48 @@ function mostrarProductos(productosArray) {
     })
 }
 
+function selecRango() {
+    
+    let inputRango = document.querySelector("#inputRango")
+    let textoRango = document.querySelector("#textoRango")
+
+    inputRango.oninput = () => {
+
+        textoRango.innerHTML = `$0 - $${inputRango.value}`
+    }
+
+    document.querySelector("#btnRango").addEventListener('click', () => {
+
+        event.preventDefault();
+
+        const productos = productosArray.filter((product) => product.precio > inputRango.value)
+
+        console.log(productos)
+
+        mostrarFiltro(productos)
+
+    })
+
+}
+
 function seleCategorias() {
 
     for (let i = 1; i < 6; i++) {
 
         document.querySelector(`#btnC${i}`).addEventListener('click', () => {
 
-            i === 1 && mostrarCategorias(celulares)
-            i === 2 && mostrarCategorias(laptops)
-            i === 3 && mostrarCategorias(procesadores)
-            i === 4 && mostrarCategorias(graficas)
-            i === 5 && mostrarCategorias(almacenamiento)
+            i === 1 && mostrarFiltro(celulares)
+            i === 2 && mostrarFiltro(laptops)
+            i === 3 && mostrarFiltro(procesadores)
+            i === 4 && mostrarFiltro(graficas)
+            i === 5 && mostrarFiltro(almacenamiento)
     
         })
         
     }
 }
 
-function mostrarCategorias(array) {
+function mostrarFiltro(array) {
 
     let divProductos = document.querySelector('#divProductos')
 
